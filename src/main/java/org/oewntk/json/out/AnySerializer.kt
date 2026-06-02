1729bd3fe94baa714cc0d7d84d1b241adbea4883
1313ou <1313ou@gmail.com>
2026-06-02 21:27:35 +0200

    do not restrict wrapper to maps

--- src/main/java/org/oewntk/json/out/AnySerializer.kt
+++ src/main/java/org/oewntk/json/out/AnySerializer.kt
@@ -18,7 +18,7 @@ import kotlin.collections.map
 import kotlin.collections.mapValues
 
 @kotlinx.serialization.Serializable
-data class SerializableWrapper(val data: Map<String, @kotlinx.serialization.Serializable(with = AnySerializer::class) Any>)
+data class SerializableWrapper(val data: @kotlinx.serialization.Serializable(with = AnySerializer::class) Any)
 
 object AnySerializer : KSerializer<Any> {
     override val descriptor: SerialDescriptor = buildClassSerialDescriptor("Any")
