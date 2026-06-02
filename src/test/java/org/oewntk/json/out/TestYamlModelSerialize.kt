2183de73326ada6d59992ff577fe7ca15303b011
1313ou <1313ou@gmail.com>
2026-06-02 08:57:03 +0200

    json tests

---
+++ src/test/java/org/oewntk/json/out/TestYamlModelSerialize.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2021-2024. Bernard Bou.
+ */
+package org.oewntk.json.out
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.junit.BeforeClass
+import org.junit.Test
+import org.oewntk.model.CoreModel
+import org.oewntk.model.Filename
+import org.oewntk.model.LibModelSubset.lexSubset
+import org.oewntk.model.LibModelSubset.synsetSubset
+import org.oewntk.model.SData
+import org.oewntk.model.toSerializable
+import org.oewntk.ser.`in`.LibTestsSerCommon.checkOrig
+import org.oewntk.ser.`in`.LibTestsSerCommon.model
+import org.oewntk.ser.`in`.LibTestsSerCommon.ps
+
+class TestYamlModelSerialize {
+
+    @OptIn(ExperimentalSerializationApi::class)
+    val json = Json {
+        prettyPrint = true
+        prettyPrintIndent = "  " // Optional: Customize indentation (default is 4 spaces)
+    }
+
+    private fun genSmallSerializable(model: CoreModel): Sequence<Pair<SData, Filename>> {
+
+        return sequence {
+            val someSerializedLexes = model.lexSubset()
+                .toSerializable(model.senseResolver)
+            yield(someSerializedLexes to "entries-some") // yield content with source file base
+
+            val someSerializedSynsets = model.synsetSubset()
+                .toSerializable()
+            yield(someSerializedSynsets to "data-some")  // yield content with source file base
+        }
+    }
+
+    @Test
+    fun testModelOneSerialization() {
+        val serialized: Sequence<Pair<SData, Filename>> = genSmallSerializable(model)
+        serialized.forEach { (sdata: SData, _: Filename) ->
+            val yamlString = json.encodeToString(sdata)
+            ps.println(yamlString)
+        }
+    }
+
+    @Test
+    fun testOrig() {
+        checkOrig()
+    }
+
+    companion object {
+
+        @JvmStatic
+        @BeforeClass
+        fun init() {
+            model //eager
+        }
+    }
+}
