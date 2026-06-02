2a49f35ff14e5609ac204810011865f9157e450b
1313ou <1313ou@gmail.com>
2026-06-02 14:16:28 +0200

    new model consumer

---
+++ src/test/java/org/oewntk/json/out/CoreModelDataConsumer.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2021-2024. Bernard Bou.
+ */
+package org.oewntk.json.out
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.oewntk.model.CoreModel
+import org.oewntk.model.dataSerialize
+import java.io.File
+import java.io.IOException
+import java.util.function.Consumer
+
+/**
+ * Main class that serializes the core model.
+ *
+ * @property outDir output dir
+ * @author Bernard Bou
+ */
+class CoreModelDataConsumer(
+    private val outDir: File,
+    val split: Boolean = true,
+    prettyPrintFlag: Boolean = true
+) : Consumer<CoreModel> {
+
+    @OptIn(ExperimentalSerializationApi::class)
+    val json = Json {
+        if (prettyPrintFlag) {
+            prettyPrint = true
+            prettyPrintIndent = "  " // default is 4 spaces
+        }
+    }
+
+    private fun jsonCoreModel(model: CoreModel, dir: File) {
+        val (dataLexes, dataSynsets, dataSenses) = model.dataSerialize()
+        val lexContent = json.encodeToString(dataLexes)
+        val synsetContent = json.encodeToString(dataSynsets)
+        val senseContent = json.encodeToString(dataSenses)
+        if (split) {
+            var file = File(dir, "oewn-synsets.json")
+            Tracing.psInfo.printf("[File] %s%n", file)
+            file.writeText(lexContent + synsetContent + senseContent)
+
+            file = File(dir, "oewn-lexes.json")
+            Tracing.psInfo.printf("[File] %s%n", file)
+            file.writeText(lexContent + synsetContent + senseContent)
+
+            file = File(dir, "oewn-senses.json")
+            Tracing.psInfo.printf("[File] %s%n", file)
+            file.writeText(lexContent + synsetContent + senseContent)
+        } else {
+            val file = File(dir, "oewn.json")
+            Tracing.psInfo.printf("[File] %s%n", file)
+            file.writeText(lexContent + "\n\n" + synsetContent + "\n\n" + senseContent)
+        }
+    }
+
+    override fun accept(model: CoreModel) {
+        Tracing.psInfo.printf("[CoreModel] %s%n", model.source)
+        if (!outDir.exists()) {
+            outDir.mkdirs()
+        }
+        try {
+            jsonCoreModel(model, outDir)
+        } catch (e: IOException) {
+            e.printStackTrace(Tracing.psErr)
+        }
+    }
+}
