b937e96da5d790816ca4bdaffc933c54c332fc5b
1313ou <1313ou@gmail.com>
2026-06-05 11:21:50 +0200

    more options

--- src/main/java/org/oewntk/json/out/oewn/CoreModelConsumer.kt
+++ src/main/java/org/oewntk/json/out/oewn/CoreModelConsumer.kt
@@ -20,11 +20,12 @@ class CoreModelConsumer(
     val split: Boolean = true,
     val fileext: String = "json",
     val generated: Boolean = false,
-    prettyPrintFlag: Boolean = false,
+    jsonMethod: JsonMethod = JsonMethod.ANY_SERIALIZER,
+    prettyPrint: Boolean = true,
     private val verbose: Boolean = false,
 ) : Consumer<CoreModel> {
 
-    val json = JsonCodec(prettyPrint = prettyPrintFlag)
+    val json = JsonCodec(jsonMethod = jsonMethod, prettyPrint = prettyPrint)
 
     private fun jsonCoreModel(model: CoreModel, dir: File) {
         if (split) {
