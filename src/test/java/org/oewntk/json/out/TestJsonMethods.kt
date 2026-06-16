1a2713ed30a61f896d5c5e189cddacc407910ee7
1313ou <1313ou@gmail.com>
2026-06-16 09:21:48 +0200

    clean up

--- src/test/java/org/oewntk/json/out/TestJsonMethods.kt
+++ src/test/java/org/oewntk/json/out/TestJsonMethods.kt
@@ -108,9 +108,9 @@ class TestJsonMethods {
 
         val ps: PrintStream = if (!silent) Tracing.psInfo else Tracing.psNull
 
-        @JvmStatic
-        @BeforeClass
-        fun init() {
-        }
+        // @JvmStatic
+        // @BeforeClass
+        // fun init() {
+        // }
     }
 }
